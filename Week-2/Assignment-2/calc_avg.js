a5c25e16bb95e05ddbf7cdae87d8f8cadf960d95
Howard <56557271+HackHow@users.noreply.github.com>
2022-06-16 00:49:26 +0800

    Update calc_avg.js

--- Week-2/Assignment-2/calc_avg.js
+++ Week-2/Assignment-2/calc_avg.js
@@ -1,1 +1,30 @@
+//Complete the function below to calculate the average price of all the products
 
+function avg(data) {
+    let total= 0;
+    for (let i = 0; i < data.size ; i++) {
+        total += data.products[i].price;
+    }
+    avgPrice = total / data.size;
+    return avgPrice;
+}
+
+console.log(
+avg({
+      size: 3,
+      products: [
+      {
+        name: 'Product 1',
+        price: 100,
+      },
+      {
+        name: 'Product 2',
+        price: 700,
+      },
+      {
+        name: 'Product 3',
+        price: 250,
+      },
+     ],
+    })
+);
